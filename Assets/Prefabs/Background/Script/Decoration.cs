1545988ed84bec6f0925442afe04e48df34ba663
Nahuel Andrada <adolfonahuel@gmail.com>
2023-06-11 16:49:24 -0300

    Stuffs

--- Assets/Prefabs/Background/Script/Decoration.cs
+++ Assets/Prefabs/Background/Script/Decoration.cs
@@ -6,16 +6,10 @@ public class Decoration : MonoBehaviour
 {
     Transform mesh = null;
     public const float REMESH_COORD = 180;
-    // Start is called before the first frame update
+
     void Start()
     {
         PickRandomMesh();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     public void PickRandomMesh()
@@ -27,30 +21,5 @@ public class Decoration : MonoBehaviour
         mesh = transform.GetChild(rnd);
         mesh.gameObject.SetActive(true);
 
-        //if (mesh) mesh.gameObject.SetActive(false);
-        //Transform meshList = transform.Find("Decorations");
-        //Debug.Log(meshList.childCount);
-        //int rnd = Random.Range(0, meshList.childCount - 1);
-        //mesh = meshList.GetChild(rnd);
-        //mesh.gameObject.SetActive(true);
-
-        // foreach (Transform child in transform)
-        // {
-        // if (child.CompareTag("Tile")) child.gameObject.SetActive(false);
-        // }
-    }
-
-    void ClearMeshes()
-    {
-
-        Transform meshList = transform.Find("Decorations");
-        int rnd = Random.Range(0, meshList.childCount - 1);
-        Transform mesh = meshList.GetChild(rnd);
-        mesh.gameObject.SetActive(true);
-
-        foreach (Transform child in transform)
-        {
-            if (child.CompareTag("Tile")) child.gameObject.SetActive(false);
-        }
     }
 }
